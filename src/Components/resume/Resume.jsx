e8375d3df37cdc52018f3659f5180ec72b27c093
Mostafa Wahba <mustafa.wahba2404@science.menofia.edu.eg>
2024-01-15 21:02:32 +0200

    mostafa

--- src/Components/resume/Resume.jsx
+++ src/Components/resume/Resume.jsx
@@ -234,89 +234,42 @@ function Resume() {
                                 <h3 className="text-uppercase pb-5 mb-0 text-left text-sm-center custom-title ft-wt-600">Experience
                                     <span>&amp;</span> Education</h3>
                             </div>
-                            <div className="col-lg-6 m-15px-tb">
-                                <div className="resume-box">
-                                    <ul>
-                                        <li>
-                                            <div className="icon">
-                                                <i className="fa fa-briefcase"></i>
-                                            </div>
-                                            <span className="time open-sans-font text-uppercase">2021 - August</span>
-                                            <h5 className="poppins-font text-uppercase">Web Development Challenger <span
-                                                className="place open-sans-font">udacity</span></h5>
-                                            <p className="open-sans-font">learn HTML , css , Wordpress and freelance work</p>
-                                            <div className='text-right'>
-                                                <NavLink to='https://graduation.udacity.com/confirm/L9ALUSQC' className="button mt-3" >
-                                                    <span className="button-text">  certificate </span>
-                                                    <span className="button-icon fa fa-download"></span>
-                                                </NavLink>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div className="icon">
-                                                <i className="fa fa-briefcase"></i>
-                                            </div>
-                                            <span className="time open-sans-font text-uppercase">2013 - 2018</span>
-                                            <h5 className="poppins-font text-uppercase">UI/UX Designer <span
-                                                className="place open-sans-font">Themeforest</span></h5>
-                                            <p className="open-sans-font">Lorem incididunt dolor sit amet, consectetur eiusmod dunt
-                                                doldunt dol elit, tempor incididunt</p>
-                                            <div className='text-right'>
-                                                <NavLink to='https://graduation.udacity.com/confirm/L9ALUSQC' className="button mt-3" >
-                                                    <span className="button-text">  certificate </span>
-                                                    <span className="button-icon fa fa-download"></span>
-                                                </NavLink>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div className="icon">
-                                                <i className="fa fa-briefcase"></i>
-                                            </div>
-                                            <span className="time open-sans-font text-uppercase">2005 - 2013</span>
-                                            <h5 className="poppins-font text-uppercase">Consultant <span
-                                                className="place open-sans-font">Videohive</span></h5>
-                                            <p className="open-sans-font">Lorem ipsum dolor sit amet, tempor incididunt ut
-                                                laboreconsectetur elit, sed do eiusmod tempor duntt</p>
-                                        </li>
-                                    </ul>
-                                </div>
+                            <div className="resume-box">
+                                <ul className='row m-0'>
+                                    <li className='col-12 col-md-6'>
+                                        <div className="icon">
+                                            <i className="fa fa-briefcase"></i>
+                                        </div>
+                                        <span className="time open-sans-font text-uppercase">2021 - August</span>
+                                        <h5 className="poppins-font text-uppercase">Web Development Challenger <span
+                                            className="place open-sans-font">udacity</span></h5>
+                                        <p className="open-sans-font">learn HTML , css , Wordpress and freelance work</p>
+                                        <div className='text-right'>
+                                            <NavLink to='https://graduation.udacity.com/confirm/L9ALUSQC' className="button mt-3" >
+                                                <span className="button-text">  certificate </span>
+                                                <span className="button-icon fas fa-images"></span>
+                                            </NavLink>
+                                        </div>
+                                    </li>
+                                    <li className='col-12 col-md-6'>
+                                        <div className="icon">
+                                            <i className="fa fa-briefcase"></i>
+                                        </div>
+                                        <span className="time open-sans-font text-uppercase">2013 - 2018</span>
+                                        <h5 className="poppins-font text-uppercase">UI/UX Designer <span
+                                            className="place open-sans-font">Themeforest</span></h5>
+                                        <p className="open-sans-font">Lorem incididunt dolor sit amet, consectetur eiusmod dunt
+                                            doldunt dol elit, tempor incididunt</p>
+                                        <div className='text-right'>
+                                            <NavLink to='https://graduation.udacity.com/confirm/L9ALUSQC' className="button mt-3" >
+                                                <span className="button-text">  certificate </span>
+                                                <span className="button-icon fas fa-images"></span>
+                                            </NavLink>
+                                        </div>
+                                    </li>
+                                </ul>
                             </div>
-                            <div className="col-lg-6 m-15px-tb">
-                                <div className="resume-box">
-                                    <ul>
-                                        <li>
-                                            <div className="icon">
-                                                <i className="fa fa-graduation-cap"></i>
-                                            </div>
-                                            <span className="time open-sans-font text-uppercase">2015</span>
-                                            <h5 className="poppins-font text-uppercase">Engineering Degree <span
-                                                className="place open-sans-font">Oxford University</span></h5>
-                                            <p className="open-sans-font">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                                                do tempor incididunt ut labore</p>
-                                        </li>
-                                        <li>
-                                            <div className="icon">
-                                                <i className="fa fa-graduation-cap"></i>
-                                            </div>
-                                            <span className="time open-sans-font text-uppercase">2012</span>
-                                            <h5 className="poppins-font text-uppercase">Master Degree <span
-                                                className="place open-sans-font">Kiev University</span></h5>
-                                            <p className="open-sans-font">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod
-                                                tempor incididunt ut adipisicing</p>
-                                        </li>
-                                        <li>
-                                            <div className="icon">
-                                                <i className="fa fa-graduation-cap"></i>
-                                            </div>
-                                            <span className="time open-sans-font text-uppercase">2009</span>
-                                            <h5 className="poppins-font text-uppercase">Bachelor Degree <span
-                                                className="place open-sans-font">Tunis High School</span></h5>
-                                            <p className="open-sans-font">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                                                do eiusmod tempor ut labore</p>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+
                         </div>
                         {/*<!-- Experience & Education Ends --> */}
                     </div>
